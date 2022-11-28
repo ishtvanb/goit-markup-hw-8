6eeaa34787401eb2b408227d076614210b67f955
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2022-11-28 11:22:43 +0200

    Додані зміни до восьмого ДЗ

---
+++ js/show.js
@@ -0,0 +1,6 @@
+// $(".button__menu").click(function () {
+//   $(".menu__container").toggleClass("active");
+// });
+$(".show").click(function () {
+  $(".menu__container.js-menu-container.is-open").removeClass("is-open");
+});
