c395e4f0a9d5ae7a1b5112fbb2ff4a7016e51f89
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2022-11-28 12:06:56 +0200

    Revert "Додані зміни до восьмого ДЗ"
    This reverts commit 6eeaa34787401eb2b408227d076614210b67f955.

--- js/show.js
+++
@@ -1,6 +0,0 @@
-// $(".button__menu").click(function () {
-//   $(".menu__container").toggleClass("active");
-// });
-$(".show").click(function () {
-  $(".menu__container.js-menu-container.is-open").removeClass("is-open");
-});
